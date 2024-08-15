cb1bc790f5f40246939698d50781d0f104956a11
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2024-08-15 15:16:46 -0500

    ...

--- src/components/Api.js
+++ src/components/Api.js
@@ -17,4 +17,16 @@ export default class Api {
   }
 
   //other methods for working with the API
+  //   User routes
+
+  // GET /users/me – Get the current user’s info
+  // PATCH /users/me – Update your profile information
+  // PATCH /users/me/avatar – Update avatar
+  // Card routes
+
+  // GET /cards – Get all cards
+  // POST /cards – Create a card
+  // DELETE /cards/:cardId – Delete a card
+  // PUT /cards/:cardId/likes – Like a card
+  // DELETE /cards/:cardId/likes – Dislike a card
 }
