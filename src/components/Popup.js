2a4d9f0a72a8ef796b9da33811a6b3a8d9090db7
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2024-09-06 11:04:46 -0500

    remove: popup.js unnecessary bind

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,7 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popupElement = document.querySelector(popupSelector);
-    this._handleEscClose = this._handleEscClose.bind(this);
+    this._handleEscClose = this._handleEscClose;
     this._closeButton = this._popupElement.querySelector(
       ".modal__close-button"
     );
